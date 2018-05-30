591d828cbfd48d1724948724d594e47254a6a36b
JIANGXIAOLONG [降小龙] <JIANGXIAOLONG@kingsoft.cn>
2018-05-30 10:22:47 +0800

    add batchnorm and tensor's tensorboard methods

--- tensorboard_demo/tensorboard_demo.py
+++ tensorboard_demo/tensorboard_demo.py
@@ -17,6 +17,26 @@ with tf.name_scope("Wa_add_b"):
     tf.summary.scalar("wight_max",tf.reduce_mean(W))
     tf.summary.scalar("b_value",tf.reduce_mean(b))
     tf.summary.scalar("a_value",tf.reduce_mean(a))
+
+    #batchNorm,eg:AssignMovingAvg,AssignMovingAvg_1,beta_0,gamma_0
+    """
+    update_ops= tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    print(update_ops)
+    if update_ops:
+        with tf.control_dependencies(update_ops):
+            grad_update = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss,global_step=global_step)
+    else:
+        grad_update = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss,global_step=global_step)
+   
+    for update_op in update_ops:
+        tf.summary.histogram(update_op.name, update_op)
+    """
+    #all train_variables,eg:tensor
+    """
+    for var in tf.trainable_variables():
+        tf.summary.histogram(var.name, var)
+    """
+
     #step:2
     merged = tf.summary.merge_all()
     train_summary = tf.summary.FileWriter('./log/',sess.graph)
